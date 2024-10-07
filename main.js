e3be39a00479780af3a37843e4613a4de4cacdeb
Gabriel Yoshitake <gyoshitaki@gmail.com>
2024-10-07 15:06:50 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,68 @@
+import './style.css'
+
+import * as THREE from 'three';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize(window.innerWidth, window.innerHeight );
+camera.position.set(0,30,30);
+
+renderer.render( scene, camera);
+
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 )
+const material = new THREE.MeshStandardMaterial ({color: 0xFF6347});
+const torus = new THREE.Mesh( geometry, material );
+torus.position.set(0,5,0)
+scene.add(torus)
+
+const pointLight = new THREE.PointLight(0xffffff,100)
+pointLight.position.set(8,8,8)
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add (pointLight, ambientLight)
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50);
+gridHelper.position.set(0,-15,0)
+scene.add(lightHelper, gridHelper)
+
+const control = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshBasicMaterial( {color: 0xffffff })
+  const star = new THREE.Mesh( geometry, material );
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ) );
+
+  star.position.set( x, y, z);
+  scene.add(star)
+}
+
+Array(200).fill().forEach(addStar)
+
+const dustTexture = new THREE.TextureLoader().load('background.jpg');
+scene.background = dustTexture;
+
+function animate() {
+  requestAnimationFrame( animate );
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.02;
+
+  control.update();
+
+  renderer.render ( scene, camera );
+}
+
+animate()
